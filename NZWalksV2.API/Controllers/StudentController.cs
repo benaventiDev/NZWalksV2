c7f0e147b6b2307d605c34b49d1b277eedf51015
Benaventi Fuentes <benaventi@techy-we.com>
2026-03-07 15:18:19 -0600

    Initial commit

---
+++ NZWalksV2.API/Controllers/StudentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalksV2.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllStudents() {
+            string[] studentNames = new string[] { "John", "Jane", "Mark", "Emily", "David"};
+            return Ok(studentNames);
+        }
+    }
+}
