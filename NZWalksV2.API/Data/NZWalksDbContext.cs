501b7e78b359c04204e4d69f170d188e64de13b7
Benaventi Fuentes <benaventi@techy-we.com>
2026-03-07 21:52:00 -0600

    Remove build artifacts from repository

---
+++ NZWalksV2.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalksV2.API.Models.Domain;
+
+namespace NZWalksV2.API.Data
+{
+    public class NZWalksDbContext: DbContext
+    {
+        public NZWalksDbContext(DbContextOptions dbContextOptions):base(dbContextOptions)
+        {
+            
+        }
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk>  Walks{ get; set; } 
+    }
+}
